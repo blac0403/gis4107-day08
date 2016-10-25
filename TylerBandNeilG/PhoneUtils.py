ced55c246797c6dd2d4bb19408b11d6246f95f1b
Tyler Black <blac0403@algonquinlive.com>
2016-10-25 16:22:27 -0400

    phone apps finished

--- TylerBandNeilG/PhoneUtils.py
+++ TylerBandNeilG/PhoneUtils.py
@@ -2,13 +2,19 @@ def main():
     test_isValidPhoneNumber()
 
 def isValidPhoneNumber(phoneNumber):
+    "Is each chunk around - a number?"
+    if len(phoneNumber) == 12:
+        if phoneNumber[0:3].isdigit():
+            if phoneNumber[4:7].isdigit():
+                if phoneNumber[8:12].isdigit():
+                    if phoneNumber[3] =='-':
+                        if phoneNumber[7]== '-':
+                            return True
 
-
+    return False
 
 def test_isValidPhoneNumber():
-    pass
+    print isValidPhoneNumber("999-999-9999")
 
 if __name__ == '__main__':
     main()
-
-[4,7][8,12]
