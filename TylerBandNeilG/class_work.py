670ba12beb65f191375926f5d7c69845463e236f
Neil Gilchrist <neil.g.gilchrist@gmail.com>
2016-10-29 12:25:37 -0400

    Oct 29th, Q6 Q7

--- TylerBandNeilG/class_work.py
+++ TylerBandNeilG/class_work.py
@@ -49,48 +49,76 @@
 ###test_getPatternPosition()
 ##
 ##
-def getInitials(fullName):
+##def getInitials(fullName):
 ##    name = fullName.split()
 ##    for letter in name:
 ##        print (letter[0] + "."),
 ##    print('')
-
-    name = fullName.split()
-    initials = ''
-    for letter in name:
-        initials += ''.join(letter[0]) + '.'
-    return initials
-
-
-def test_getInitials():
-    print getInitials("John Samuel Wobbly")
-    print getInitials("Dylan Mcdermott")
-    print getInitials("Nora Young")
-
-#test_getInitials()
-
-def isValidPhoneNumber(phoneNumber):
-##    "Is each chunk around - a number?"
-##    if len(phoneNumber) == 12:
-##        if phoneNumber[0:3].isdigit():
-##            if phoneNumber[4:7].isdigit():
-##                if phoneNumber[8:12].isdigit():
-##                    if phoneNumber[3] =='-':
-##                        if phoneNumber[7]== '-':
-##                            return True
 ##
+##    name = fullName.split()
+##    initials = ''
+##    for letter in name:
+##        initials += ''.join(letter[0]) + '.'
+##    return initials
+##
+##
+##def test_getInitials():
+##    print getInitials("John Samuel Wobbly")
+##    print getInitials("Dylan Mcdermott")
+##    print getInitials("Nora Young")
+
+###test_getInitials()
+
+##def isValidPhoneNumber(phoneNumber):
+##    if len(phoneNumber) ==12:
+##        if phoneNumber[0:3:7].isdigit() and phoneNumber[8:12].isdigit():
+##            pass
+##        if phoneNumber[3] and phoneNumber[7] =='-':
+##            return True
 ##    return False
+##
+##
+##def test_isValidPhoneNumber():
+##    print isValidPhoneNumber("999-999-9999")
+##
+###test_isValidPhoneNumber()
+##
+##def phoneNumberHasLetters(phoneNumber):
+##    if len(phoneNumber) == 12:
+##        if phoneNumber [3] and phoneNumber[7] == '-':
+##            pass
+##        if phoneNumber[0:3].isdigit():
+##            pass
+##        if (phoneNumber[4:7:12].isalpha()) == 1:
+##            return True
+##    return False
+##
+##def test_phoneNumberHasLetters():
+##    print phoneNumberHasLetters("999-A99-9999")
+##    print phoneNumberHasLetters("999-A99-BB99")
+##    print phoneNumberHasLetters("999-999-9999")
+##    print phoneNumberHasLetters("9A9-999-9999")
+##
+##test_phoneNumberHasLetters()
+##
+##def getFeatureTypeFromName(fcName):
+##    if fcName.upper().endswith('_PNT'):
+##        return "Point"
+##    if fcName.upper().endswith('_LIN'):
+##        return "Line"
+##    if fcName.upper().endswith('_PLY'):
+##        return "Polygon"
+##    return "Unknown"
+##
+##def test_getFeatureTypeFromName():
+##    print getFeatureTypeFromName("adc_PNT")
+##    print getFeatureTypeFromName("adasdc_LIN")
+##    print getFeatureTypeFromName("adasdasdasdc_PLY")
+##    print getFeatureTypeFromName("adc_pnt")
+##    print getFeatureTypeFromName("adasdc_lin")
+##    print getFeatureTypeFromName("adasdasdasdc_ply")
+##    print getFeatureTypeFromName("adasd.ply")
+##    print getFeatureTypeFromName("adasdasdasdc_y")
+##
+##test_getFeatureTypeFromName()
 
-
-    if len(phoneNumber) ==12:
-        if phoneNumber[0:3:7].isdigit() and phoneNumber[8:12].isdigit():
-            pass
-        if phoneNumber[3] and phoneNumber[7] =='-':
-            return True
-    return False
-
-
-def test_isValidPhoneNumber():
-    print isValidPhoneNumber("999-999-9999")
-
-test_isValidPhoneNumber()
